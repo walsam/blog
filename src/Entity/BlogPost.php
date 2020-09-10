80c01abc4533fb3b64b72bb249f5cb0c4514e7eb
Walid Maarad <walsam@hotmail.com>
2020-09-10 18:23:50 +0100

    Using is_granted() to limit some stuff to only authenticated users, and set the author inside blog_posts automatically.

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -7,12 +7,18 @@ use App\Repository\BlogPostRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=BlogPostRepository::class)
  * @ApiResource(
  *     itemOperations={"get"},
- *     collectionOperations={"get"}
+ *     collectionOperations={
+ *            "get",
+ *            "post"={
+ *                 "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
+ *             }
+ *     }
  * )
  */
 class BlogPost
@@ -26,16 +32,22 @@ class BlogPost
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\NotBlank()
+     * @Assert\Length(min="10")
      */
     private $title;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\NotBlank()
+     * @Assert\DateTime()
      */
     private $published;
 
     /**
      * @ORM\Column(type="text")
+     * @Assert\NotBlank()
+     * @Assert\Length(min="20")
      */
     private $content;
 
@@ -47,6 +59,7 @@ class BlogPost
 
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
+     * @Assert\NotBlank()
      */
     private $slug;
 
