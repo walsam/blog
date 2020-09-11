d726ccd8ee41febdef721b5aeaeef1bcb4a29878
Walid Maarad <walsam@hotmail.com>
2020-09-11 14:14:04 +0100

    set the current date for blog_posts and comments via EventSubscriber

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -7,6 +7,7 @@ use App\Repository\BlogPostRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 
@@ -25,6 +26,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
  *            "post"={
  *                 "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
  *             }
+ *     },
+ *     denormalizationContext={
+ *         "groups"={"post"}
  *     }
  * )
  */
@@ -41,12 +45,12 @@ class BlogPost implements AuthoredEntityInterface, PublishedDateEntityInterface
      * @ORM\Column(type="string", length=255)
      * @Assert\NotBlank()
      * @Assert\Length(min="10")
+     * @Groups({"post"})
      */
     private $title;
 
     /**
      * @ORM\Column(type="datetime")
-     * @Assert\NotBlank()
      * @Assert\DateTime()
      */
     private $published;
@@ -55,6 +59,7 @@ class BlogPost implements AuthoredEntityInterface, PublishedDateEntityInterface
      * @ORM\Column(type="text")
      * @Assert\NotBlank()
      * @Assert\Length(min="20")
+     * @Groups({"post"})
      */
     private $content;
 
@@ -67,6 +72,7 @@ class BlogPost implements AuthoredEntityInterface, PublishedDateEntityInterface
     /**
      * @ORM\Column(type="string", length=255, nullable=true)
      * @Assert\NotBlank()
+     * @Groups({"post"})
      */
     private $slug;
 
--- src/Entity/Comment.php
+++ src/Entity/Comment.php
@@ -6,6 +6,8 @@ use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\CommentRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 
 /**
@@ -21,6 +23,9 @@ use Symfony\Component\Security\Core\User\UserInterface;
  *            "post"={
  *                 "access_control"="is_granted('IS_AUTHENTICATED_FULLY')"
  *             }
+ *     },
+ *     denormalizationContext={
+ *         "groups"={"post"}
  *     }
  * )
  * @ORM\Entity(repositoryClass=CommentRepository::class)
@@ -36,11 +41,15 @@ class Comment implements AuthoredEntityInterface, PublishedDateEntityInterface
 
     /**
      * @ORM\Column(type="text")
+     * @Groups({"post"})
+     * @Assert\NotBlank()
+     * @Assert\Length(min=5, max=3000)
      */
     private $content;
 
     /**
      * @ORM\Column(type="datetime")
+     * @Assert\DateTime()
      */
     private $published;
 
