622df8b846d5a129e8180492485d35297e5c0044
Walid Maarad <walsam@hotmail.com>
2020-09-12 13:31:13 +0100

    added some Roles to users (a user can have multiple roles)

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -50,6 +50,14 @@ use Symfony\Component\Validator\Constraints as Assert;
  */
 class User implements UserInterface
 {
+    const ROLE_COMMENTATOR = "ROLE_COMMENTATOR";
+    const ROLE_WRITER = "ROLE_WRITER";
+    const ROLE_EDITOR = "ROLE_EDITOR";
+    const ROLE_ADMIN = "ROLE_ADMIN";
+    const ROLE_SUPERADMINE = "ROLE_SUPERADMINE";
+
+    const DEFAULT_ROLE = [self::ROLE_COMMENTATOR];
+
     /**
      * @ORM\Id
      * @ORM\GeneratedValue
@@ -116,10 +124,16 @@ class User implements UserInterface
      */
     private $comments;
 
+    /**
+     * @ORM\Column(type="simple_array", length=200)
+     */
+    private $roles;
+
     public function __construct()
     {
         $this->posts = new ArrayCollection();
         $this->comments = new ArrayCollection();
+        $this->roles = self::DEFAULT_ROLE;
     }
 
     public function getId(): ?int
@@ -191,9 +205,14 @@ class User implements UserInterface
         return $this->comments;
     }
 
-    public function getRoles()
+    public function getRoles() : array
     {
-        return ['ROLE_USER'];
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles)
+    {
+        $this->roles = $roles;
     }
 
     public function getSalt()
