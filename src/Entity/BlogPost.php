2afe655c1794a7ab763dd87722513cdbe319e67a
Walid Maarad <walsam@hotmail.com>
2020-09-14 15:15:46 +0100

    feat: added more filters to our blog_posts entity

--- src/Entity/BlogPost.php
+++ src/Entity/BlogPost.php
@@ -13,16 +13,42 @@ use Symfony\Component\Serializer\Annotation\Groups;
 use Symfony\Component\Validator\Constraints as Assert;
 use Symfony\Component\Security\Core\User\UserInterface;
 use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\DateFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\RangeFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\OrderFilter;
+use ApiPlatform\Core\Serializer\Filter\PropertyFilter;
 
 /**
  * @ORM\Entity(repositoryClass=BlogPostRepository::class)
  * @ApiFilter(
  *     SearchFilter::class,
  *     properties={
-            "id": "exact",
  *          "title": "partial",
  *          "content": "partial",
- *          "author": "exact"
+ *          "author": "exact",
+ *          "author.name": "partial"
+ *     }
+ * )
+ * @ApiFilter(
+ *     DateFilter::class,
+ *     properties={"published"}
+ * )
+ * @ApiFilter(RangeFilter::class, properties={"id"})
+ * @ApiFilter(
+ *     OrderFilter::class,
+ *     properties={
+            "id",
+ *          "published",
+ *          "title"
+ *     },
+ *     arguments={"orderParameterName"="_order"}
+ * ),
+ * @ApiFilter(
+ *     PropertyFilter::class,
+ *     arguments={
+            "parameterName":"properties",
+ *          "overrideDefaultProperties": false,
+ *          "whitelist": {"id", "author", "slug", "title", "content"}
  *     }
  * )
  * @ApiResource(
