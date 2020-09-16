871ab8ea3f394ef0485c2c945be5c9df8ac27664
Walid Maarad <walsam@hotmail.com>
2020-09-16 15:16:27 +0100

    test: finishing all the tests on AuthoredEntitySubscriber, and fixing it to pass the tests.

--- src/EventSubscriber/AuthoredEntitySubscriber.php
+++ src/EventSubscriber/AuthoredEntitySubscriber.php
@@ -37,8 +37,14 @@ class AuthoredEntitySubscriber implements EventSubscriberInterface
         $entity = $event->getControllerResult();
         $method = $event->getRequest()->getMethod();
 
-        /**@var UserInterface $author */
-        $author = $this->tokenStorage->getToken()->getUser();
+        $token = $this->tokenStorage->getToken();
+
+        if (null === $token) {
+            return;
+        }
+
+        /** @var UserInterface $author */
+        $author = $token->getUser();
 
         if (!$entity instanceof AuthoredEntityInterface || Request::METHOD_POST !== $method){
             return;
